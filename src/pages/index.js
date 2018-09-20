dd6d152bc5b33b452235b483cf564e29721cf118
Adriaan van Swieten <adriaan@Adriaans-MacBook-Pro.local>
2018-09-20 18:12:24 -0400

    added test links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,8 @@ const IndexPage = (props) => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p><Link to="/page-2/">Go to page 2</Link></p>
+    <p><Link to="/en">This link does work</Link></p>
   </Layout>
 )
 
