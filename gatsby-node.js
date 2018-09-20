ba0ab9a8b52afdd8e833586db5fc10f77d4e9eae
Adriaan van Swieten <adriaan@Adriaans-MacBook-Pro.local>
2018-09-20 16:01:08 -0400

    filtered out settings content types

--- gatsby-node.js
+++ gatsby-node.js
@@ -31,18 +31,41 @@ exports.createPages = ({ graphql, actions }) => {
           console.log(result.errors)
           reject(result.errors)
         }
-
         const entries = result.data.allStoryblokEntry.edges;
-        console.log(entries);
-        entries.forEach((entry, index) => {
-          createPage({
-            path: `/${entry.node.full_slug}`,
-            component: storyblokEntry,
-            context: {
-              story: entry.node
-            }
-          })
+        let enGlobalSetting = [];
+        let cnGlobalSetting = [];
+        for (let i = 0; i < entries.length; i++) {
+          if (entries[i].node.full_slug === "en/globalsettings") {
+            enGlobalSetting = entries.splice(i,1)
+          }
+          else if (entries[i].node.full_slug === "cn/globalsettings") {
+            cnGlobalSetting = entries.splice(i,1)
+          }
+  } ;
+
+  console.log(entries);
+  entries.forEach((entry, index) => {
+    if (entry.node.full_slug.substring(0,3) == "en/") {
+      createPage({
+        path: `/${entry.node.full_slug}`,
+        component: storyblokEntry,
+        context: {
+          globalSettings: enGlobalSetting,
+          story: entry.node
+        }
+      })
+    }
+      else if (entry.node.full_slug.substring(0,3) == "cn/") {
+        createPage({
+          path: `/${entry.node.full_slug}`,
+          component: storyblokEntry,
+          context: {
+            globalSettings: cnGlobalSetting,
+            story: entry.node
+          }
         })
+      }
+  })
       })
     )
   })
